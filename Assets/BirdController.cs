171b515d8f3ff142b6a9d788f57f14207f090fbd
Justin-bit25 <justinr777@nycstudents.net>
2025-05-20 09:24:33 -0400

    done

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdController : MonoBehaviour
 { 
     Rigidbody2D bird;
+    int score = 0;
+    public Text scoreUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,17 @@ public class BirdController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             bird.AddForce(new Vector2(0,1) * 200);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Points"))
+        {
+            // Increase score by 1
+            score = score + 1;
+
+            scoreUI.text = score.ToString();
         }
     }
 }
